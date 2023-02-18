4da92a5d9cbf27b987f03830f28b0e88254bf3a8
Ruan <ruanmoraes012@gmail.com>
2023-02-18 11:37:15 -0300

    Criação de HTML

---
+++ TokenBus/app/urls.py
@@ -0,0 +1,16 @@
+# from django.contrib import admin
+# from django.urls import path
+
+# urlpatterns = [
+#    path('admin/', admin.site.urls),
+# ]
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.homepage),
+    path("menu", views.menu),
+    path("cadastro", views.cadastro),
+    path("login", views.login),
+    path("buscador", views.buscador)
+]
---
+++ TokenBus/app/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+
+
+# from django.http import HttpResponse
+
+
+# Create your views here.
+def start(request):
+    return render(request, 'test.html')
+    # HttpResponse("Meu primeiro response html em django")
+
+
+def login(request):
+    return render(request, 'login.html')
+
+
+def cadastro(request):
+    return render(request, 'cadastro.html')
+
+
+def homepage(request):
+    return render(request, 'home.html')
+
+
+def menu(request):
+    return render(request, 'menu.html')
+
+
+def buscador(request):
+    return render(request, 'buscador.html')
+
+
