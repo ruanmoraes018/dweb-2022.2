ba5c7d133eb33f69daa4dae0dd9a3c0de929289e
Ruan <ruanmoraes012@gmail.com>
2023-03-19 17:20:00 -0300

    Arquivos Bootstrap (PT 2)

--- TokenTravel/TokenTravel/wsgi.py
+++ TokenTravel/TokenTravel/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for TokenBus project.
+WSGI config for TokenTravel project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -12,6 +12,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling, MediaCling
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TokenBus.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TokenTravel.settings')
 
 application = Cling(MediaCling(get_wsgi_application()))
